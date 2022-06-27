8b014a00a6350eeaeb8cd573892a1eabd50b7c46
+ <+>
2022-06-26 20:51:27 -0400

    commit tree c9fba98643ded7f96faee96de476fa23c4599a56

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name){
+
+cats.push ("Ralph")
+
+}
+
+function destructivelyPrependCat(name){
+
+    cats.unshift ("Bob")
+    
+}
+
+function destructivelyRemoveLastCat(name){
+
+    cats.pop ()
+    
+}
+
+function destructivelyRemoveFirstCat(name){
+
+    cats.shift ()
+    
+}
+function appendCat(name){
+    cats;
+    const appendCat = [... cats]
+    appendCat.push("Broom");
+    return appendCat;
+    
+}
+function prependCat(name){
+    cats;
+    const appendCat = [... cats]
+    appendCat.unshift("Arnold");
+    return appendCat;
+    
+}
+function removeLastCat(name){
+    cats;
+    const appendCat = [... cats]
+    appendCat.pop();
+    return appendCat;
+    
+}
+function removeFirstCat(name){
+    cats;
+    const appendCat = [... cats]
+    appendCat.shift();
+    return appendCat;
+    
+}
